9ffe01cbda175ae78a29cd6d12dfb11e1ec07b93
yash6102000 <yash.thakur@copods.co>
2026-02-08 19:27:28 +0530

    skelton-added

--- apps/next/pages/events/index.tsx
+++ apps/next/pages/events/index.tsx
@@ -82,6 +82,12 @@ export default function Events() {
             <EventCategoriesSkeleton />
             <ArtistCarouselSkeleton />
           </Box>
+        ) : loading ? (
+          <Box gap="$16">
+            <EventCarouselSkeleton />
+            <EventCategoriesSkeleton />
+            <ArtistCarouselSkeleton />
+          </Box>
         ) : (
           <Box>
             <Navbar />
